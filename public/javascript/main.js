2ed8e482011e02929d95c7d3d759df90212ab668
Michael Pospisil <michaelcpospisil@gmail.com>
2015-04-01 23:13:05 -0700

    adding responses

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,27 +1,17 @@
-// var natural = require('natural'),
-// tokenizer = new natural.WordTokenizer();
 
-//THIS IS WHAT I WOULD LIKE TO USE!!!
-   //=================================
-   // var INPUTtoken = tokenizer.tokenize(INPUT);
-   // console.log(INPUTtoken);
-   //=================================
-
-// for(var x=0;x<INPUT.length;x++){
-//   console.log(INPUT[x]);
-//      checkArrays(INPUT[x]);
-// }
 //=================================================================
 //THESE ARRAYS HOLD THE POSSIBLE USER INPUTS
 //=================================================================
 
 keywords = ['def', 'flick', 'loc', 'ask', 'yoda', 'lyric'];
-symbols = ['!', '?', '/'];
-greetings = ['hello', 'hi', 'yo'];
+pro_sentences = ['yes', 'no', 'maybe', 'affirmative', 'negative', 'yup', 'yeah'];
+greetings = ['hello', 'hi', 'yo', 'morn', 'afternoon', 'hei', 'wuddup', 'sup', 'howdy', 'greet'];
 goodbyes = ['goodbye', 'later', 'bye'];
 insults = ['bore', 'stupid', 'lam', 'dickwad', 'fatass', 'honkei', 'jagoff', 'shiznit', 'twat', 'wank', 'tard', 'nutsack', 'pecker', 'arse', 'dick', 'dickbag', 'dickfac', 'dickhead', 'dickwe', 'poonani'];
 swearWords = ['ass', 'assbag', 'assclown', 'assfuck', 'asshat', 'asshol', 'asslick', 'bastard', 'bitch', 'bitchass', 'bitchtit', 'boner', 'cock', 'cockass', 'cockfac', 'cockhead', 'cockmonkei', 'cooter', 'cumdumpst', 'cunt', 'cumslut', 'cuntass', 'cuntfac', 'dipshit', 'dooshbag', 'douchebag', 'dumbass', 'dumass', 'dumbfuck', 'dumbshit', 'dumshit', 'fuck', 'fuckass', 'fuckbag', 'fuckboi', 'fuckbrain', 'fucker', 'fuckfac', 'fuckhead', 'fuckhol', 'fucknut', 'fucknutt', 'fuckoff', 'fuckstick', 'fucktard', 'fucktart', 'fuckwad', 'fuckwit', 'fuckwitt', 'ho', 'hoe', 'jackass', 'jerkass', 'lameass', 'mothafucka', 'motherfucker', 'peckerhead', 'peni', 'prick', 'pussi', 'puto', 'queef', 'shit', 'shitass', 'shitass', 'shitbag', 'shitbagger', 'shitbrain', 'shitbrain', 'shitcunt', 'shitdick', 'shitfac', 'shithead', 'shithol', 'shitstain', 'skank', 'slut', 'slutbag', 'suckass', 'tit', 'titfuck', 'vagina', 'whore', 'whorebag'];
-arrayList = [keywords, symbols, greetings, goodbyes, insults, swearWords];
+howRUBad = ['ok', 'alrit', 'alright', 'okai', 'fine', 'good', 'meh', 'not', 'better', 'noth'];
+howRUGood = ['awesom', 'super', 'great', 'perfect', 'prime', 'ideal', 'magnific', 'splendid', 'marvel', 'superb', 'delight'];
+arrayList = [keywords, pro_sentences, greetings, goodbyes, insults, swearWords, howRUBad, howRUGood];
 //=================================================================
 var INPUT;
 
@@ -30,10 +20,13 @@ $("#commForm").submit(function(e){
   $("#reply").append($('<li>').text(localUsername + " : " + $("#command").val()));
   $("#command").text(" ");
   console.log("questionFormat" + " >> " + $("#command").val().split(" ").slice(1).join("+"));
-  console.log("mapSearchFormat" + " >> " +$("#command").val().split(" ").slice(1).join(" "));
+  console.log("mapSearchFormat" + " >> " + $("#command").val().split(" ").slice(1).join(" "));
   $questionFormat = $("#command").val().split(" ").slice(1).join("+");
   $mapSearchFormat = $("#command").val().split(" ").slice(1).join(" ");
-console.log($("#command").val().split(" ").slice(1));
+  $rawINPUT = $("#command").val().split(" ");
+  console.log("rawINPUT = " + $rawINPUT);
+
+// console.log($("#command").val().split(" ").slice(1));
 
 //THIS GETS THE CONVERTED INPUT FROM THE SERVER SIDE
 //=================================================================
@@ -44,29 +37,48 @@ console.log($("#command").val().split(" ").slice(1));
     inputReady(INPUT);
   });
 //=================================================================
-
+//THESE LOOPS AND CONDITIONALS ACCOUNT FOR WORDS THAT NATURAL OMITS DURING TOKENIZATION
+//=================================================================
+for(var w=0;w<$rawINPUT.length;w++) {
+  if ($rawINPUT[w] === "you?" ) {
+    console.log("HIT How are you");
+    howAIisReply();
+  } else if ($rawINPUT[w] === "you") {
+    howAIisReply();
+  }
+}
+for(var r=0;r<$rawINPUT.length;r++) {
+  if ($rawINPUT[0] === "why" ) {
+    console.log("HIT How are you");
+    whyReply();
+  } else if ($rawINPUT[0] === "why?") {
+    whyReply();
+  } else if ($rawINPUT[0] === "because")
+    whyReply();
+}
 //=================================================================
 //THE FOLLOWING LOOPS THROUGH THE RESPONSE ARRAYS AND COMPARES THE USER INPUT
 //DECIDES WHICH OUTPUT FUNCTION TO CALL DEPENDING ON ARRAY MATCHES
 //=================================================================
-//THE INPUTREADY FUNCTION SEPERATES COMMANDS WHICH INCLUDE A KEYWORD AT THE BEGINNING FROM
+//THE INPUT READY FUNCTION SEPERATES COMMANDS WHICH INCLUDE A KEYWORD AT THE BEGINNING FROM
 //THOSE WHICH DON'T AND CALL THE APPROPRIATE FUNCTIONS
   function inputReady (INPUT) {
-    var anything = false;
+    var isKeyword = false;
     console.log(INPUT);
     for(var x=0;x<keywords.length;x++) {
         console.log("the keyword is", INPUT[0]);
           if(keywords[x] === INPUT[0]) {
-            anything = true;
+            isKeyword = true;
             console.log(x);
             console.log("keyword array");
             $matchedWord = keywords[x];
           }    
     }//END OF KEYWORDS FOR-LOOP
       
-    if (anything) {
+    if (isKeyword) {
       console.log($matchedWord);
       KEYWORD();
+      isKeyword = false;
     } else {
       INPUT.forEach(function(element) {
         for(var i=0;i<arrayList.length;i++) {
@@ -94,8 +106,12 @@ console.log($("#command").val().split(" ").slice(1));
               } else if (i === 5) {
                 console.log("swearWords array");
                 curses();
-              } else if (arrayList[i][j] !== element) {
-                noCompute();
+              } else if (i === 6) {
+                console.log("howRUBad array");
+                howRUBadReply();
+              } else if (i === 7) {
+                console.log("howRUGood");
+                howRUGoodReply();
               }
             }
 
@@ -172,7 +188,6 @@ function symbol() {
 
 
 //=================================================================
-
 //REPLY FUNCTIONS
 //=================================================================
 function noCompute() {
@@ -230,25 +245,91 @@ function insult() {
 function curses() {
   num = Math.random();
   console.log(num);
-  if (0.5 > num > 0) {
+  if (0.25 > num > 0) {
     $("#reply").append($('<li>').text('You should not swear..'));
-    // document.getElementById("glow").style.visibility = "visible";
-    setTimeout(function(){
+        setTimeout(function(){
       $("#reply").append($('<li>').text('you' + ' ' + $matchedWord));
       document.getElementById("glow").style.visibility = "visible";
+      setTimeout(function(){turnEyeOff();}, 2000);
     }, 2500);
 
-  } else if (1 > num > 0.5) {
+  } else if (0.5 > num > 0.25) {
      $("#reply").append($('<li>').text('Well that was not called for...'));
-    // document.getElementById("glow").style.visibility = "visible";
     setTimeout(function(){
       $("#reply").append($('<li>').text('you' + ' ' + $matchedWord));
       document.getElementById("glow").style.visibility = "visible";
+      setTimeout(function(){turnEyeOff();}, 2000);
+    }, 2500);
+  } else if (0.75 > num > 0.5) {
+    $("#reply").append($('<li>').text('You are not very nice you know.'));
+    setTimeout(function(){
+      $("#reply").append($('<li>').text('In fact, you are the ' + $matchedWord));
+      document.getElementById("glow").style.visibility = "visible";
+      setTimeout(function(){turnEyeOff();}, 2000);
+    }, 2500);
+  } else if (1 > num > 0.75) {
+    $("#reply").append($('<li>').text('That was pretty mean.'));
+    setTimeout(function(){
+      $("#reply").append($('<li>').text('I think you are the ' + $matchedWord));
+      document.getElementById("glow").style.visibility = "visible";
+      setTimeout(function(){turnEyeOff();}, 2000);
     }, 2500);
   }
+  function turnEyeOff() {
+    document.getElementById("glow").style.visibility = "hidden";
+  }
 
-    
     $("#command").val("");
+}
+
+
+function howRUBadReply() {
+  num = Math.random();
+  console.log(num);
+  if (0.5 > num > 0) {
+    $("#reply").append($('<li>').text('just ' + $rawINPUT.join() + '?'));
+  } else if (1 > num > 0.5) {
+    $("#reply").append($('<li>').text('I am sorry to hear that..'));
+  }
+  $("#command").val("");
+}
+
+function howRUGoodReply() {
+  num = Math.random();
+  if (0.5 > num > 0) {
+    $("#reply").append($('<li>').text('are you really ' + $matchedWord + '?'));
+  } else if (1 > num > 0.5) {
+    $("#reply").append($('<li>').text('I am glad to hear that!'));
+  }
+  $("#command").val("");
+
+}
+
+function howAIisReply () {
+  num = Math.random();
+  if (0.25 > num > 0) {
+    $("#reply").append($('<li>').text('You know what ' + localUsername + '. I am pretty damn good.'));
+  } else if (0.5 > num > 0.25) {
+    $("#reply").append($('<li>').text('I am functioning within established parameters, ' + localUsername));
+  } else if (0.75 > num > 0.5) {
+    $("#reply").append($('<li>').text('Well...'));
+    setTimeout(function(){
+      $("#reply").append($('<li>').text('I am stuck in your computer, so.. as good as I can be..'));
+    }, 2500);
+  } else if (1 > num > 0.75) {
+    $("#reply").append($('<li>').text('Terrible. Just Terrible...'));
+  }
+  $("#command").val("");
+
+}
+
+function whyReply () {
+  if (0.5 > num > 0) {
+    $("#reply").append($('<li>').text('Why do you think?'));
+  } else if (1 > num > 0.5) {
+    $("#reply").append($('<li>').text('Because.'));
+  }
+  $("#command").val("");
 }
 //=================================================================
 
@@ -322,17 +403,19 @@ function searchDictionary() {
 }
 
 
-function searchLyrics() {
-  console.log($mapSearchFormat.split(" "));
-  var searchArr = $("#command").val().split(" ").slice(1);
-  var artistSong = {artistSong: searchArr};
-  $.get( '/lyrics', artistSong, function (data) {
-    console.log(data);
-    console.log(typeof(data.body), data.body);
-    $("#reply").append($('<li>').text(data.body));
-  });
+// function searchLyrics() {
+//   console.log($mapSearchFormat.split(" "));
+//   var searchArr = $("#command").val().split(" ").slice(1);
+//   var artistSong = {artistSong: searchArr};
+//   $.get( '/lyrics', artistSong, function (data) {
+//     console.log(data);
+//     console.log(typeof(data.body), data.body);
+//     $("#reply").append($('<li>').text(data.body));
+//   });
 
-}
+//USE SPLIT AND SPLICE TO GET THE VALUE YOU WANT
+
+// }
 //GOOGLE TEXT TO SPEECH HACK
 //=================================================================
 // url = "http://translate.google.com/translate_tts?tl=en&q=speak%22";
@@ -393,6 +476,7 @@ eyeFlicker[0].addEventListener("mouseout", endflicker);
 // GOOGLE MAP MODAL 
 //=================================================================
 function showMap () {
+  $("#command").val("");
   var url = encodeURI("https://maps.googleapis.com/maps/api/geocode/json?address=" + $mapSearchFormat + "&key=AIzaSyDE6F79FbnrSc9hZlurECTyBJoEyHCj-Nc");
   $.getJSON(url, function(data) {
         // console.log(data);
@@ -468,34 +552,34 @@ function xmlParser (xml) {
 
 //DISPLAYS A RANDOM QUESTION AT A "SET INTERVAL"
 //=================================================================
-setInterval(function showQuest() {
-$(document).ready(function () {
-$.ajax({
-    type: "GET",
-    url: "popQuestions.xml",
-    dataType: "xml",
-    success: xmlParser
-   });
-});
-function xmlParser (xml) {
-  console.log(xml);
-  $(xml).find("suggestionset").each(function () {
-    var TEXT = $(this).find("text");
-    console.log($(this).find("text").first());
-    console.log(TEXT);
-    var decimal = Math.random() * (962 - 1) + 1;
-    var number =  Math.round(decimal);
-    console.log(number);
-    console.log(TEXT[0]);
-    console.log(typeof(TEXT[number].innerHTML), TEXT[number].innerHTML);
-    var temp = TEXT[number].innerHTML;
-    console.log(temp);
-    // $("#reply").append($('<li>').text(TEXT[number].innerHTML));
-    $("#randQuest").append(TEXT[number].innerHTML).hide().fadeIn(700); 
-  });
-}
-$("#randQuest").text(" ");
-}, 5000);
+// setInterval(function showQuest() {
+// $(document).ready(function () {
+// $.ajax({
+//     type: "GET",
+//     url: "popQuestions.xml",
+//     dataType: "xml",
+//     success: xmlParser
+//    });
+// });
+// function xmlParser (xml) {
+//   console.log(xml);
+//   $(xml).find("suggestionset").each(function () {
+//     var TEXT = $(this).find("text");
+//     console.log($(this).find("text").first());
+//     console.log(TEXT);
+//     var decimal = Math.random() * (962 - 1) + 1;
+//     var number =  Math.round(decimal);
+//     console.log(number);
+//     console.log(TEXT[0]);
+//     console.log(typeof(TEXT[number].innerHTML), TEXT[number].innerHTML);
+//     var temp = TEXT[number].innerHTML;
+//     console.log(temp);
+//     // $("#reply").append($('<li>').text(TEXT[number].innerHTML));
+//     $("#randQuest").append(TEXT[number].innerHTML).hide().fadeIn(700); 
+//   });
+// }
+// $("#randQuest").text(" ");
+// }, 5000);
 //=================================================================
 
 
